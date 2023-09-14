e515e8150b08449854fbfea7a03bdc23bd19ebf9
Crowdbotics <team@crowdbotics.com>
2023-09-14 16:16:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "grama-43389",
+  url: "https://grama-43389.botics.co",
+  api: "https://grama-43389.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
